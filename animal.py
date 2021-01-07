7c4195aa63c782c6790941c850a0e605a4edfe13
Mohammed Ibrahim <mohammed.ibrahim.abdelazeez@gmail.com>
2021-01-07 07:22:46 -0800

    Adding the goat() function

--- animal.py
+++ animal.py
@@ -9,11 +9,16 @@ def dog():
 def cat():
 	print("I am a cat")
 
+def goat():
+	print("This is a goat")
+
 def main():
 	if sys.argv[1] == 'cat':
 		cat()
 	elif sys.argv[1] == 'dog':
 		dog()
+	elif sys.argv[1] == 'goat':
+		goat()
 	else:
 		animal()
 
